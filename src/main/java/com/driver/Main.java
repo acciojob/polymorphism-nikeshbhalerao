e5bd13fdcae7258fdf6cd1edce5b810e7890215f
Nikeshbhalerao123 <nikeshbhalerao1998@gmail.com>
2023-02-09 21:34:35 +0530

    In second attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,24 +5,28 @@ public class Main {
     public static class product {
 
         public int product(int x , int y){
-            return x+y;
+            return x*y;
         }
 
         public int product(int x , int y , int z){
-            return x+y+z;
+            return x*y*z;
         }
 
         public double product(double x, double y){
-            return x+y;
+            return x*y;
         }
 
     }
 
     public static void main(String [] args){
         product  p = new product();
-        System.out.println(p.product(10,20));
-        System.out.println(p.product(10,20,30));
-        System.out.println(p.product(10.5,20.5));
+        p.product(10,20);
+        p.product(10,20,30);
+        p.product(20.3,10.3);
+
+//        System.out.println(p.product(10,20));
+//        System.out.println(p.product(10,20,30));
+//        System.out.println(p.product(10.5,20.5));
     }
 
 }
