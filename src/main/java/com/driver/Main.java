68c62433be16c97cd6626b02fb1b0dde09850c0c
Nikeshbhalerao123 <nikeshbhalerao1998@gmail.com>
2023-02-09 18:08:56 +0530

    In first attemp

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,27 @@ package com.driver;
 
 public class Main {
 
+    public static class product {
+
+        public int product(int x , int y){
+            return x+y;
+        }
+
+        public int product(int x , int y , int z){
+            return x+y+z;
+        }
+
+        public double product(double x, double y){
+            return x+y;
+        }
+
+    }
+
+    public static void main(String [] args){
+        product  p = new product();
+        System.out.println(p.product(10,20));
+        System.out.println(p.product(10,20,30));
+        System.out.println(p.product(10.5,20.5));
+    }
+
 }
